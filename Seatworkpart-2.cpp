1259a44b6d687e94de25318828cd69235400ea43
RealubitCPE <qsarealubit@tip.edu.ph>
2026-07-08 13:16:03 +0800

    Add files via upload

---
+++ Seatworkpart-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int price_1, price_2, price_3;
+    int payment, change;
+
+    cout << "Price of item 1: ";
+    cin >> price_1;
+
+    cout << "Price of item 2: ";
+    cin >> price_2;
+
+    cout << "Price of item 3: ";
+    cin >> price_3;
+
+    cout >> "Payment: ";
+    cin >> payment
+}
