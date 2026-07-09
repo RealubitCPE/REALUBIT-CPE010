aa0f166c104bc02bbac24adad60ad97b40ea46aa
RealubitCPE <qsarealubit@tip.edu.ph>
2026-07-10 06:54:57 +0800

    Add files via upload

--- Seatworkpart-2.cpp
+++ Seatworkpart-2.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main(){
-
+    // Cashiering
     int price_1, price_2, price_3;
-    int payment, change;
+    int payment, change, total_price;
 
     cout << "Price of item 1: ";
     cin >> price_1;
@@ -15,6 +15,25 @@ int main(){
     cout << "Price of item 3: ";
     cin >> price_3;
 
-    cout >> "Payment: ";
-    cin >> payment
+    cout << "Payment: ";
+    cin >> payment;
+
+    total_price = price_1 + price_2 + price_3;
+    change = payment - total_price;
+
+    // Transaction Receipt
+    cout << "\n Transaction Receipt " << endl;
+    cout << "Item 1: " << price_1 << endl;
+
+    cout << "Item 2: " << price_2 << endl;
+
+    cout << "Item 3: " << price_3 << endl;
+    
+    cout << "Total: " << total_price << endl;
+
+    cout << "Payment: " << payment << endl;
+
+    cout << "Change: " << change << endl;
+
+    return 0;
 }
